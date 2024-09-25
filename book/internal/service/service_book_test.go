03ba8e0ba2d3aa8cdcd1025d01a86ccaa43c6eb0
AMIR_bA <53426863+amirbaa1@users.noreply.github.com>
2024-09-25 23:14:51 +0330

    add test

--- book/internal/service/service_book_test.go
+++ book/internal/service/service_book_test.go
@@ -1,1 +1,47 @@
 package service
+
+import (
+	"book/internal/core/model"
+	"book/mocks"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCreateBookService(t *testing.T) {
+	// mockeService := new(mocks.BookService)
+	mockBookRepository := new(mocks.BookRepository) // استفاده از mock برای repository
+
+
+	bookCreate := &model.RequestBook{
+		Title:     "Test",
+		FirstName: "TestMamad",
+		LastName:  "Taster",
+	}
+
+	author := &model.Author{
+		Id:        uuid.New(),
+		FirstName: bookCreate.FirstName,
+		LastName:  bookCreate.LastName,
+	}
+
+
+	
+	mockBookRepository.On("FindAuthorByFullNameForCreate", bookCreate.FirstName, bookCreate.LastName).Return(author, nil).Once()
+	mockBookRepository.On("CreateBook", mock.Anything).Return(nil).Once()
+
+	service := &Service{
+		bookRepository: mockBookRepository,
+	}
+
+
+	// err := mockeService.CreateServiceBook(bookCreate)
+	err:= service.CreateServiceBook(bookCreate)
+
+	assert.NoError(t, err)
+
+	mockBookRepository.AssertExpectations(t)
+
+}
