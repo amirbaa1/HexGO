5c7e50537897c2aabb54b298e6b4839a840fbe3e
amirbaa1 <amir.baahklag@yahoo.com>
2024-09-27 00:01:04 +0330

    update

--- book/internal/service/service_book_test.go
+++ book/internal/service/service_book_test.go
@@ -1,19 +1,20 @@
 package service
 
 import (
-	"book/internal/core/model"
-	"book/mocks"
+	"errors"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+
+	"book/internal/core/model"
+	"book/mocks"
 )
 
 func TestCreateBookService(t *testing.T) {
 	// mockeService := new(mocks.BookService)
 	mockBookRepository := new(mocks.BookRepository) // استفاده از mock برای repository
-
 
 	bookCreate := &model.RequestBook{
 		Title:     "Test",
@@ -27,8 +28,6 @@ func TestCreateBookService(t *testing.T) {
 		LastName:  bookCreate.LastName,
 	}
 
-
-	
 	mockBookRepository.On("FindAuthorByFullNameForCreate", bookCreate.FirstName, bookCreate.LastName).Return(author, nil).Once()
 	mockBookRepository.On("CreateBook", mock.Anything).Return(nil).Once()
 
@@ -36,12 +35,33 @@ func TestCreateBookService(t *testing.T) {
 		bookRepository: mockBookRepository,
 	}
 
-
 	// err := mockeService.CreateServiceBook(bookCreate)
-	err:= service.CreateServiceBook(bookCreate)
+	err := service.CreateServiceBook(bookCreate)
 
 	assert.NoError(t, err)
 
 	mockBookRepository.AssertExpectations(t)
 
+}
+
+func TestCreateService_error(t *testing.T) {
+	mockRespository := new(mocks.BookRepository)
+
+	bookCreate := &model.RequestBook{
+		Title:     "Test",
+		FirstName: "TestMamad",
+		LastName:  "Taster",
+	}
+
+	mockRespository.On("FindAuthorByFullNameForCreate", bookCreate.FirstName, bookCreate.LastName).Return(nil, errors.New("database error")).Once()
+
+	service := &Service{bookRepository: mockRespository}
+
+	err := service.CreateServiceBook(bookCreate)
+
+	assert.Error(t, err)
+
+	assert.EqualError(t, err, "database error")
+
+	mockRespository.AssertExpectations(t)
 }
